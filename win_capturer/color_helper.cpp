f5bb9bdeba42baa4d86377361543591258d53a9c
linxing <linxing@52tt.com>
2024-10-16 19:42:43 +0800

    颜色空间定义

--- win_capturer/color_helper.cpp
+++ win_capturer/color_helper.cpp
@@ -1,28 +1,23 @@
 #include "win_capturer/color_helper.h"
 
+#include <libyuv.h>
+
 int ConvertVideoType(DeviceColorOuput ouput_type) {
     switch (ouput_type) {
     case DeviceColorOuput::MF_NONE:
         return libyuv::FOURCC_ANY;
     case DeviceColorOuput::MF_I420:
         return libyuv::FOURCC_I420;
-    case VideoType::kIYUV:  // same as VideoType::kYV12
-    case VideoType::kYV12:
-        return libyuv::FOURCC_YV12;
-    case VideoType::kRGB24:
-        return libyuv::FOURCC_24BG;
-    case VideoType::kRGB565:
-        return libyuv::FOURCC_RGBP;
-    case VideoType::kYUY2:
+    case DeviceColorOuput::MF_IYUV:
+        return libyuv::FOURCC_IYUV;
+    case DeviceColorOuput::MF_YUY2:
         return libyuv::FOURCC_YUY2;
-    case VideoType::kUYVY:
-        return libyuv::FOURCC_UYVY;
-    case VideoType::kMJPEG:
+    //case DeviceColorOuput::MF_YVYU:
+    //    return libyuv::FOURCC_UYVY;
+    case DeviceColorOuput::MF_MJPG:
         return libyuv::FOURCC_MJPG;
-    case VideoType::kARGB:
-        return libyuv::FOURCC_ARGB;
-    case VideoType::kBGRA:
-        return libyuv::FOURCC_BGRA;
+    case DeviceColorOuput::MF_NV12:
+        return libyuv::FOURCC_NV12;
     }
     //RTC_DCHECK_NOTREACHED();
     return libyuv::FOURCC_ANY;
